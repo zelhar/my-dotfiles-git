a596fdd46e5e4c42edcb33f283b7ba783fe51abc
Yiftach Kolb <zelhar@gmail.com>
2020-05-14 22:14:26 +0200

    S

--- dotfiles_syn_in.sh
+++ dotfiles_syn_in.sh
@@ -3,4 +3,5 @@ rsync -tPvr --delete-excluded --delete --ignore-errors \
     --files-from='./dotfileslist.txt' \
     --exclude '.git/*' \
     --exclude '.cache/' \
+    --exclude '.netrwhist' \
     /home/zelhar ./usr_home/
