6f5b0cad3afd2da191501649c18f34f260759503
Yiftach Kolb <zelhar@gmail.com>
2020-05-27 00:18:36 +0200

    S

--- dotfiles_syn_in.sh
+++ dotfiles_syn_in.sh
@@ -5,3 +5,7 @@ rsync -tPvr --delete-excluded --delete --ignore-errors \
     --exclude '.cache/' \
     --exclude '.netrwhist' \
     /home/zelhar ./usr_home/
+
+#backup installed packages list
+pacman -Qe > package_list.txt
+conda list -n test > conda_test_env_package_list.txt
